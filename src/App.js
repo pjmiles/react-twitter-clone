5f887979461e1eca643016f40bd681c4216f2f23
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-11 19:04:54 +0100

    hook created for AuthContext

--- src/App.js
+++ src/App.js
@@ -9,36 +9,24 @@ import SignUp from "./components/signup/SignUp";
 import { useState, createContext } from "react";
 
 function App() {
-  const [logDetails, setLogDetails] = useState({ username: "", password: "" });
-  const [authUser, setAuthUser] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  // const [logDetails, setLogDetails] = useState({ username: "", password: "" });
+  // const [authUser, setAuthUser] = useState(false);
+  // const [openModal, setOpenModal] = useState(false);
 
-  const Authenticated = createContext(authUser);
+  // const Authenticated = createContext(authUser);
 
   return (
     <div className="app">
-      <Authenticated.Provider value={{ authUser }}>
+      {/* <Authenticated.Provider value={{ authUser }}> */}
           <>
-            {/* <SideBar /> */}
             <Routes>
+              <Route path="/" element={<LoginForm logged={localStorage.getItem("logDetails")}/>} />
               <Route path="/home" element={<Feed />} />
               <Route path="/profile" element={<ProfileHeader />} />
-              <Route path="/" element={<LoginForm logged={localStorage.getItem("logDetails")}/>} />
             </Routes>
             <Widgets />
-          </>
-        {/* {authUser ? (
-        ) : (
-          <>
-            <LoginForm
-              logDetails={logDetails}
-              setLogDetails={setLogDetails}
-              setAuthUser={setAuthUser}
-            />
-          </>
-        )} */}
-        {/* <SignUp openModal={openModal} setOpenModal={setOpenModal} /> */}
-      </Authenticated.Provider>
+          </> 
+      {/* </Authenticated.Provider> */}
     </div>
   );
 }
---
+++ src/hook/AuthContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// creating the context
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState("user", null);
+  const navigate = useNavigate();
+
+  // function to authenticate user if logged in
+  const login = async (data) => {
+    setUser(data);
+    navigate("/profile");
+  };
+
+  // function to be called when user is not logged in
+  const logout = () => {
+    setUser(null);
+    navigate("/", { replace: true });
+  };
+
+  const value = useMemo(() => ({ user, login, logout }), [user]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+// using the context created
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
